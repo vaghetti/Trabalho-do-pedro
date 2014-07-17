af20ef8d4bf24615ac82bb109231621c4100ac6b
Aluno <aluno@21xy-zz.(none)>
2014-07-17 14:35:30 -0300

    AAAAAAAAAAAAAA

--- gerenciadorArquivos.cpp
+++ gerenciadorArquivos.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int blocosPorINode=8;
 
 struct INode{
-    char nome[64];
+    char nome[32];
     bool diretorio; //false para arquivo, true para diretorio
     time_t ultimaModificacao;
     short int referencias;
@@ -12,16 +12,36 @@ struct INode{
     int pai;
     int enderDireto[blocosPorINode];
     int enderIndireto;
+    char teste[4];
     bool expansao;
+    int grupo;
+    int dono;
+    bool permissoes[9];
 };
 
-const int tamDisco=128*128; //128 é 1k de bytes
-const int tamBloco=8; //8 bytes
-const int tamINodes=64*sizeof(INode);  //128 é 1k de bytes
-const int tamGerVazioInodes=tamINodes/sizeof(INode);
-const int temp=(tamDisco-tamINodes-tamGerVazioInodes)/tamBloco;
-const int tamEspacoBlocos=tamDisco-temp-tamGerVazioInodes-tamINodes;
-const int tamGerVazioBlocos = (tamEspacoBlocos - temp)/tamBloco;
+struct Usuario{
+    char nome[32];
+    char senha[32];
+};
+
+struct Grupo{
+    char nome[32];
+    int membro[4];
+};
+
+const int tamDisco=32*1024;
+const int tamBloco= 8;
+const int numeroInodes = 128;
+const int numeroBlocos = 64;
+const int numeroUsuarios=16;
+const int espacoUsuarios=numeroUsuarios*sizeof(Usuario);
+const int grupos=8;
+const int espacoGrupos=grupos*sizeof(Grupo);
+const int tamINodes= sizeof(INode)*numeroInodes;
+const int tamGerVazioInodes = numeroInodes;
+const int tamGerVazioBlocos=numeroBlocos;
+const int tamEspacoBlocos= numeroBlocos*tamBloco;
+
 
 
 int INODESCRIADOS=0;
@@ -47,11 +67,12 @@ char* getBloco(int bloco);
 
 int makeINode(bool dir,char * nome,bool expansao){
     int pos=findInodeLivre();
-    printf("deu merda aqui1\n");
+    //printf("deu merda aqui1\n");
     INode *novo = getINode(pos);
-    printf("deu merda aqui2\n");
+    printf("ponteiro novo : %p\n",novo);
+    //printf("deu merda aqui2\n");
     *getMapInode(pos)=true;
-    printf("deu merda aqui3\n");
+    //printf("deu merda aqui3\n");
     //char temp[]="/";
     strcpy(novo->nome,nome);
     printf("deu merda aqui3.5\n");
@@ -60,30 +81,57 @@ int makeINode(bool dir,char * nome,bool expansao){
     printf("deu merda aqui4\n");
     novo->pai=diretorioAtual;
 
-    printf("deu merda aqui5\n");
+    //printf("deu merda aqui5\n");
     time(&novo->ultimaModificacao);
 
     limpaReferencias(pos);
-    printf("deu merda aqui6\n");
+    //printf("deu merda aqui6\n");
     if(pos!=0)  //se for a raiz não tem pai
         adicionaReferencia(novo->pai,pos);
     novo->expansao=expansao;
 
-    printf("deu merda aqui7\n");
+    //printf("deu merda aqui7\n");
     novo->tamanho=0;
     return pos;
 }
 
+bool nomeUsado(char* nome ){
+    INode* inode= getINode(diretorioAtual);
+    while(true){
+        for(int x=0;x<blocosPorINode;x++){
+            if(inode->enderDireto[x]!=-1){
+                if(strcmp(getINode(inode->enderDireto[x])->nome,nome)==0){
+                    return true;
+                }
+            }
+        }
+        if(inode->enderIndireto!=-1){
+            inode=getINode(inode->enderIndireto);
+        }else{
+            break;
+        }
+    }
+    return false;
+}
+
 void mkdir(char * nome){
-    int pos=makeINode(true,nome,false);
-    getINode(getINode(pos)->pai)->tamanho++;
-    printf("criou diretorio %s\n",getINode(pos)->nome);
+    if(!nomeUsado(nome)){
+        int pos=makeINode(true,nome,false);
+        getINode(getINode(pos)->pai)->tamanho++;
+        printf("criou diretorio %s\n",getINode(pos)->nome);
+    }else{
+        printf("este nome já foi usado\n");
+    }
 }
 
 void touch(char* nome){
-    int pos=makeINode(false,nome,false);
-    getINode(getINode(pos)->pai)->tamanho++;
-    printf("criou arquivo %s\n",getINode(pos)->nome);
+    if(!nomeUsado(nome)){
+        int pos=makeINode(false,nome,false);
+        getINode(getINode(pos)->pai)->tamanho++;
+        printf("criou arquivo %s\n",getINode(pos)->nome);
+    }else{
+        printf("este nome já foi usado\n");
+    }
 }
 
 void removeInode(int pos){
@@ -108,14 +156,14 @@ void echo(char * texto,char*nome){
                 int novo=makeINode(false,(char*)"expansao",true);
                 inode->enderIndireto=novo;
                 inode=getINode(novo);
-                printf("\n trocou inode \n");
+                //rintf("\n trocou inode \n");
             }
             bloco=findBlocoLivre();
             *getMapBlocos(bloco)=true;
             adicionaReferencia(pos,bloco);  //aloca um bloco novo quando encher o anterior e cria a referencia para ele
-            printf("\n trocou bloco\n");
+            //printf("\n trocou bloco\n");
         }
-        printf("%c",texto[c]);
+        //printf("%c",texto[c]);
         *(getBloco(bloco)+c%tamBloco)=texto[c];
     }
 }
@@ -127,11 +175,11 @@ void cat(char* nome){
         if(i%tamBloco==0){
             buffer=getBloco(inode->enderDireto[blocoAtual]);
             blocoAtual++;
-            printf("\n troca bloco \n");
+            //printf("\n troca bloco \n");
             if(blocoAtual==blocosPorINode){
                 blocoAtual=0;
                 inode=getINode(inode->enderIndireto);
-                printf("\n troca inode \n");
+                //printf("\n troca inode \n");
             }
         }
         printf("%c",*(buffer+i%blocosPorINode));
@@ -283,8 +331,8 @@ char * geraArvoreDiretorio(){
 }
 
 bool* getMapInode(int posicao){
-    posicao=tamGerVazioBlocos + posicao;
-    return (bool*) disco+posicao;
+    int aux=tamGerVazioBlocos + posicao;
+    return (bool*) disco+aux;
 }
 
 bool*getMapBlocos(int posicao){
@@ -351,14 +399,28 @@ char* getBloco(int bloco){
         return NULL;
     }
     int posicao=tamGerVazioBlocos+tamGerVazioInodes+tamINodes+tamBloco*bloco;
+    printf("retornando posicao %d no getbloco\n",posicao);
     return disco+posicao;
 }
 
+Usuario* getUsuario(int posicao){
+    return disco+tamGerVazioBlocos+tamGerVazioInodes+sizeof(Usuario)*posicao;
+}
+
 void printInfoDisco(){
-    printf("Tamanho do disco :%d\ngerencimento dos blocos %d\ninodes:%d\nVazio dos inodes %d\nEspaço para os blocos %d\n",tamDisco,tamGerVazioBlocos,tamINodes,tamGerVazioInodes,tamEspacoBlocos);
+    printf("Tamanho do disco :%d\ngerencimento dos blocos %d\ninodes:%d\nVazio dos inodes %d\nEspaço para os blocos %d\n Tamanho do inode %d\n",tamDisco,tamGerVazioBlocos,tamINodes,tamGerVazioInodes,tamEspacoBlocos,sizeof(INode));
+}
+
+
+
+int login(char * nome){
+    for(int x=0;numeroUsuarios;x++){
+
+    }
 }
 
 int main(){
+    printf("tamanho de um bool %d, time %d , short int %d , inode %d\n",sizeof(bool),sizeof(time_t), sizeof(short int),sizeof(INode));
     printInfoDisco();
     printf("Bem vindo ao sistema de arquivos!\nInforme seu nome: ");
     scanf("%s",nomeUsuario);
@@ -372,53 +434,68 @@ int main(){
         printf("Arquivo aberto com sucesso\n");
     }
     criaSistemaDeArquivos();
+    printf("blocos : \n");
+    for(int cont = 0; cont < tamGerVazioBlocos; cont++){
+        printf(" %p = %d\n",getMapBlocos(cont),*getMapBlocos(cont));
+    }
+
+    printf("inodes: \n");
+    for(int cont = 0; cont < tamGerVazioInodes; cont++){
+        printf(" %p = %d\n",getMapInode(cont),*getMapInode(cont));
+    }
 
     //disco=(char)malloc(tamDisco);
     //disco=mmap
     char entrada[256];
     int teste=10;
     while(true){
-        printf("%s@MacBook Pro Retina Display 50': %s~$ ",nomeUsuario,geraArvoreDiretorio());  //FIXME!!! nao sei pq mas quando tem isso fica num printf infinito
-        scanf(" %s",entrada);
-        printf("leu entrada %s\n",entrada);
-        if(strcmp(entrada,(char*)"mkdir")==0){
+        scanf("%s",entrada);
+        if(strcmp(entrada,(char*)"login")){
             scanf(" %s",entrada);
-            mkdir(entrada);
         }
 
-        if(strcmp(entrada,"touch")==0){
+        while(true){
+            printf("%s@MacBook Pro Retina Display 50': %s~$ ",nomeUsuario,geraArvoreDiretorio());  //FIXME!!! nao sei pq mas quando tem isso fica num printf infinito
             scanf(" %s",entrada);
-            touch(entrada);
-        }
-        if(strcmp(entrada,(char*)"ls")==0){
-            ls();
-        }
-        if(strcmp(entrada,(char*)"cd")==0){
-            scanf(" %s",entrada);
-            cd(entrada);
-        }
-        if(strcmp(entrada,(char*)"pwd")==0){
-            printf("/%s\n",geraArvoreDiretorio());
-        }
-        if(strcmp(entrada,(char*)"rmdir")==0){
-            scanf(" %s",entrada);
-            rmdir(entrada);
-        }
-        if(strcmp(entrada,(char*)"rm")==0){
-            scanf(" %s",entrada);
-            rmdir(entrada);
-        }
-        if(strcmp(entrada,(char*)"echo")==0){
-            char* nome=(char*)malloc(512);
-            scanf(" %s > %s",entrada,nome);
-            echo(entrada,nome);
-            free(nome);
-        }
-        if(strcmp(entrada,(char*)"cat")==0){
-            scanf(" %s",entrada);
-            cat(entrada);
-        }
+            printf("leu entrada %s\n",entrada);
+            if(strcmp(entrada,(char*)"mkdir")==0){
+                scanf(" %s",entrada);
+                mkdir(entrada);
+            }
 
+            if(strcmp(entrada,"touch")==0){
+                scanf(" %s",entrada);
+                touch(entrada);
+            }
+            if(strcmp(entrada,(char*)"ls")==0){
+                ls();
+            }
+            if(strcmp(entrada,(char*)"cd")==0){
+                scanf(" %s",entrada);
+                cd(entrada);
+            }
+            if(strcmp(entrada,(char*)"pwd")==0){
+                printf("/%s\n",geraArvoreDiretorio());
+            }
+            if(strcmp(entrada,(char*)"rmdir")==0){
+                scanf(" %s",entrada);
+                rmdir(entrada);
+            }
+            if(strcmp(entrada,(char*)"rm")==0){
+                scanf(" %s",entrada);
+                rmdir(entrada);
+            }
+            if(strcmp(entrada,(char*)"echo")==0){
+                char* nome=(char*)malloc(512);
+                scanf(" %s > %s",entrada,nome);
+                echo(entrada,nome);
+                free(nome);
+            }
+            if(strcmp(entrada,(char*)"cat")==0){
+                scanf(" %s",entrada);
+                cat(entrada);
+            }
+        }
     }
     printf("bugou aqui\n");
 }
