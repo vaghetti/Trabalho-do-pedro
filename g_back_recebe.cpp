aaa785773c79ba1b79e0f974415383b0d26aad86
Hallan <hallanneves@gmail.com>
2014-09-25 15:57:25 -0300

    Update g_back_recebe.cpp
    updade

--- g_back_recebe.cpp
+++ g_back_recebe.cpp
@@ -5,53 +5,49 @@
 #include "pedro.h"
 #define PORT 50000
 #define MAXSIZE 256
-#define QUADROS 100000
-int fdSocket; //Descritor do socket do cliente ou do socket efetivo do servidor
+#define QUADROS 10
+#define TIMEOUT 10
 
-void* enviadorConfirm(void * socket){
-	char message[MAXSIZE];
-	int fdSocket = (int) socket;
-	while(true){
-		if(!strcmp(message, "tchau"))
-			break;
-		usleep(100);
-	}
-	printf("acabou enviador \n");
-}
+long fdSocket; //Descritor do socket do cliente ou do socket efetivo do servidor
 
-void * recebedorDados(void * socket){
+void * recebedorDados(void * lixo){
 	char message[MAXSIZE];
-	int fdSocket = (int) socket;
+
 	while(true){
 		receiveMessage(fdSocket,message,sizeof(int));
-		if((rand()%100)>75 ){
-			sendMessage(fdSocket,message,sizeof(int)); //Envia mensagem
+		if((rand()%100)<95 ){
+			printf("recebeu %s \n",message);
+			if((rand()%100)<90 ){
+				sendMessage(fdSocket,message,sizeof(int)); //Envia mensagem
+				printf("confirmou %s \n",message);
+			}else{
+				printf("falha na confirmação\n");
+			}
 		}
-		usleep(10000);
 	}
 	printf("acabou recebedor \n");
 }
 
 int main(int argc, char *argv[]){
 
-	int mainSocket; //Descritor do socket principal do servidor
 	srand(time(NULL));
 	//Estruturas auxiliares do socket
 	struct sockaddr_in socketAddr;
 	socklen_t sockLen;
 
 	char message[MAXSIZE]; 	//String para troca de mensagens
-	pthread_t threadRecebedor,threadEnviador;
+	pthread_t threadRecebedor;
 	//Se for servidor
 
-	fdSocket=tryConnection((char*)"localhost", PORT, 0); //Tenta conectar ao servidor
-	gets(message);
+	fdSocket=openConnection(PORT,0);	//Abre conexão principal
+	fdSocket=acceptConnection(fdSocket, sockLen); //Espera por conexões
+
+	scanf("%s",message);
 	
-	pthread_create(&threadRecebedor,NULL,&recebedorDados,(void*)fdSocket);
-	pthread_create(&threadEnviador,NULL,&enviadorConfirm,(void*)fdSocket);
+	pthread_create(&threadRecebedor,NULL,&recebedorDados,NULL);
+
 	pthread_join(threadRecebedor,NULL);
-	pthread_join(threadEnviador,NULL);
-	
+	usleep(100000);
 	closeConnection(fdSocket); //Fecha socket com o servidor
 
 	return 0;
