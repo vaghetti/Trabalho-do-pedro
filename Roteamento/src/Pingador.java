a3f545ee1a2e0df212eab5b2620ccc8e0ac0dc28
Hallan <hallanneves@gmail.com>
2014-10-20 17:17:01 -0300

    Adiciona o Roteamento
    É isso ai mesmo

---
+++ Roteamento/src/Pingador.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+
+//Thread que fica pedidndo o ping de cada um dos vizinhos
+public class Pingador extends Thread {
+	String[] IPsVizinhos;
+	private final int tempoEspera=5;
+	public void run(){
+		try {
+			while(true){	
+				for(int x=0;x<IPsVizinhos.length;x++){
+					new AtualizaPing(IPsVizinhos[x]).start();  //manda o ip do vizinho para ober o novo ping
+				}
+				Thread.sleep(tempoEspera*1000);
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		} 	
+	}
+	public Pingador (String[] IPsVizinhos){
+		this.IPsVizinhos=IPsVizinhos;
+	}
+}
---
+++ Roteamento/src/RespondedorPing.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//Thread que fica recebendo requisicoes de ping e disparando Threads para responde-las
+
+public class RespondedorPing extends Thread{
+    private ServerSocket servidor; //abre a porta
+	public void run(){
+		while(true){
+			try {
+				Socket conexao = servidor.accept();
+				new RespondePing(conexao).start();  //dispara Thread para esperar um tempo aleatorio, responder o pedido e fechar a conexao
+			} catch (IOException e) {
+				e.printStackTrace();
+			}		
+		}
+	}
+	public RespondedorPing() throws IOException{
+		 servidor = new ServerSocket(Principal.portaIP);	 
+	}
+}
