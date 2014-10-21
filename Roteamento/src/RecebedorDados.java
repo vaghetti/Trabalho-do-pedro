f795fd6f5423d0b7b69ccc59ad8c6733875615ad
Hallan <hallanneves@gmail.com>
2014-10-20 22:03:35 -0300

    nao tem tempo para mensagens

--- Roteamento/src/RecebedorDados.java
+++ Roteamento/src/RecebedorDados.java
@@ -60,7 +60,7 @@ public class RecebedorDados extends Thread {
                     }
                     tempo = Long.parseLong(par[1]);
                     //System.out.println("RECEBEDORDADOS: parser leu tempo :"+tempo+" e peso "+ peso+" de "+vetorIPs[x]+ " ate "+vetorIPs[y]);
-                    if(Principal.pesos[Principal.mapaIPs.get(vetorIPs[x])][Principal.mapaIPs.get(vetorIPs[y])][1] > tempo && peso!=Long.MAX_VALUE){
+                    if(Principal.pesos[Principal.mapaIPs.get(vetorIPs[x])][Principal.mapaIPs.get(vetorIPs[y])][1] < tempo && peso!=Long.MAX_VALUE){
                        Principal.pesos[Principal.mapaIPs.get(vetorIPs[x])][Principal.mapaIPs.get(vetorIPs[y])][1] = tempo;
                        Principal.pesos[Principal.mapaIPs.get(vetorIPs[x])][Principal.mapaIPs.get(vetorIPs[y])][0] = peso;
                        System.out.println("RECEBEDORDADOS: atualizou o peso de "+vetorIPs[x]+" ate "+vetorIPs[y]+" para "+peso);
