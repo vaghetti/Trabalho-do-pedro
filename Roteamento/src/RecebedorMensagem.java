c661da5831110e5134dbc68f37dd4adf2141dfa2
Aluno <aluno@21xy-zz.(none)>
2014-10-21 15:57:11 -0200

    nao ha tempo para mensagens

---
+++ Roteamento/src/RecebedorMensagem.java
@@ -0,0 +1,67 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import javax.lang.model.type.PrimitiveType;
+
+/*
+ * PROTOCOLO DE MENSAGEM
+ * IPRECEBER:IPENVIOU:MENSAGEM
+ */
+public class RecebedorMensagem extends Thread {
+    private ServerSocket servidor;
+    private BufferedReader input;
+    private PrintWriter out ;
+    private Socket conexao;
+    public void run(){
+        try {
+            while(true){
+                Socket conexao = servidor.accept();
+                input =new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+                String aux=input.readLine();
+                String mensagem[] = aux.split(":");
+                
+                if(mensagem[0].equals(Principal.say())){
+                    System.out.println("RECEBEDORMENSAGEM: recebeu a mensagem "+mensagem[2]+" de "+mensagem[1]);
+                }else{
+                    synchronized(Dijkstrador.anterior){
+                        int anterior = Dijkstrador.anterior[Principal.mapaIPs.get(mensagem[0])];
+                        Socket repassa = null;
+                        if(anterior==0){  //se esta em um dos vizinhos do computador que deve receber a mensagem
+                            repassa = new Socket(mensagem[0],Principal.portaMensagens);
+                            System.out.println("RECEBEDORMENSAGEM: encaminhou mensagem"+mensagem[2]+" para "+mensagem[0]);
+                        }else{ //se nao for um dos vizinhos
+                            while(Dijkstrador.anterior[anterior]!=0){
+                                anterior = Dijkstrador.anterior[anterior];
+                            }
+                            for(Map.Entry<String, Integer> entry : Principal.mapaIPs.entrySet()) {
+                                String key = entry.getKey();
+                                int value = entry.getValue();
+                                if(value == anterior){
+                                    System.out.println("RECEBEDORMENSAGEM: encaminhou mensagem "+mensagem[2]+" de "+mensagem[0]+" para "+mensagem[0] + "atravez de "+key);
+                                    repassa = new Socket(key,Principal.portaMensagens);
+                                    break;
+                                }
+                            }
+                        }
+                        out =new PrintWriter(repassa.getOutputStream(), true);
+                        out.println(aux);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public RecebedorMensagem(){
+        try {
+            servidor=new ServerSocket(Principal.portaMensagens);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
